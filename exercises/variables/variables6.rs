8ac1486f6ee17f7ff81d7ea2eff5c10438503bdf
kei32bit <keinishino78@gmail.com>
2021-09-12 11:50:08 +0900

    init

---
+++ exercises/variables/variables6.rs
@@ -0,0 +1,7 @@
+// variables6.rs
+// Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
+
+const NUMBER:u16 = 3;
+fn main() {
+    println!("Number {}", NUMBER);
+}
