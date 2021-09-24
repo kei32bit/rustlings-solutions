24ee7bcfd652cc0099c4d28f88a4a10c367abd90
kei32bit <keinishino78@gmail.com>
2021-09-24 11:30:18 +0900

    answered try_from_into

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -12,8 +12,6 @@ struct Color {
     blue: u8,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
 // You need to create an implementation for a tuple of three integers,
@@ -26,19 +24,56 @@ struct Color {
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = Box<dyn error::Error>;
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {}
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (red, green, blue) = tuple;
+        if red < 0 || red > 255 || green < 0 || green > 255 || blue < 0 || blue > 255 {
+                return Err(String::from("rgb should be 0~255").into())
+        } else {
+            Ok(Color {
+                red: u8::try_from(red)?,
+                green: u8::try_from(green)?,
+                blue: u8::try_from(blue)?,
+            })
+        }
+    }
 }
 
 // Array implementation
 impl TryFrom<[i16; 3]> for Color {
     type Error = Box<dyn error::Error>;
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {}
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        match arr.len() {
+            3 => if arr[0] < 0 || arr[0] > 255 || arr[1] < 0 || arr[1] > 255 || arr[2] < 0 || arr[2] > 255 {
+                return Err(String::from("rgb should be 0~255").into())
+            } else {
+                Ok(Color {
+                    red: arr[0].try_into()?,
+                    green: arr[1].try_into()?,
+                    blue: arr[2].try_into()?
+                })
+            }
+            _ => Err("should 3 color variables".into()),
+        }
+    }
 }
 
 // Slice implementation
 impl TryFrom<&[i16]> for Color {
     type Error = Box<dyn error::Error>;
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {}
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        match slice.len() {
+            3 => if slice[0] < 0 || slice[0] > 255 || slice[1] < 0 || slice[1] > 255 || slice[2] < 0 || slice[2] > 255 {
+                return Err(String::from("rgb should be 0~255").into())
+            } else {
+                Ok(Color {
+                    red: slice[0].try_into()?,
+                    green: slice[1].try_into()?,
+                    blue: slice[2].try_into()?
+                })
+            }
+            _ => Err("should 3 color variables".into()),
+        }
+    }
 }
 
 fn main() {
