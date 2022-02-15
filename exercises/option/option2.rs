1c47eca5cbb53efd5ec2b355a2228b647277f507
tarekadam <tarekadam@users.noreply.github.com>
2022-02-15 08:44:29 -0600

    asked for while loop

--- exercises/option/option2.rs
+++ exercises/option/option2.rs
@@ -17,7 +17,10 @@ fn main() {
 
     // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
     // You can stack `Option<T>`'s into while let and if let
-    if let integer = optional_integers_vec.pop() {
+    while let integer = optional_integers_vec.pop() {
+        if(integer.is_none()){
+            break
+        }
         println!("current value: {:?}", integer);
     }
 }
